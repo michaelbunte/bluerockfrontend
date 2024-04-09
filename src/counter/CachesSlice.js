fbf37b30b4f365cdd23e8ebee79b99864143f000
Michael Bunte <michaelwbunte@gmail.com>
2024-04-09 12:23:18 -0700

    minor reformatting

--- src/counter/CachesSlice.js
+++ src/counter/CachesSlice.js
@@ -16,6 +16,12 @@ export const cachesSlice = createSlice({
     }
 });
 
-export const { set_selected_sensors_cache_to_loaded } = cachesSlice.actions;
-export const select_selected_sensors_cache_state = (state) => state.caches.selected_sensors_cache_state;
+export const {
+    set_selected_sensors_cache_to_loading,
+    set_selected_sensors_cache_to_loaded
+} = cachesSlice.actions;
+
+export const select_selected_sensors_cache_state = 
+    (state) => state.caches.selected_sensors_cache_state;
+
 export default cachesSlice.reducer;
